4bb69ae4bd0d394a19384a2b3a4a02319e18e3dc
NatashaMelnik <n.melnik1788@gmail.com>
2021-01-15 14:35:04 +0330

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    toDoList: []
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <h1>To Do List</h1>
+        </div>
+        <form className="newEl" onSubmit={this.handleSubmit}>
+          <div className='inputT'>
+            <input type="text" name='toDoTask' className='form-control' placeholder='enter task' autoComplete='off' />
+            <button type='submit' className='addButton'>Add</button>
+          </div>
+        </form>
+
+        <ul className='list'>
+          {
+            this.state.toDoList.map(
+              (item, index) => {
+                return <li key={index}>
+                  {item}
+                  <button className='del-button' onClick={(event) => { this.deleteTask(event, index) }}>Del</button>
+                </li>
+              }
+            )
+          }
+        </ul>
+      </div>
+    );
+
+  }
+  handleSubmit = (event) => {
+    var taskText = event.target.elements.toDoTask.value;
+    if (taskText.length > 0) {
+      this.setState({
+        toDoList: [...this.state.toDoList, taskText]
+      })
+      event.target.reset();
+    }
+    event.preventDefault();
+  }
+
+  deleteTask = (event, index) => {
+    var taskArr = [...this.state.toDoList];
+    taskArr.splice(index, 1);
+    this.setState({ toDoList: taskArr });
+  }
+
 }
 
 export default App;
